e4ff36d129712906393d03e82da040b5f5e76ac2
JP-DEV-0303 <105988568+JP-DEV-0303@users.noreply.github.com>
2023-02-21 16:18:58 -0500

    Update enemy.py

--- enemy.py
+++ enemy.py
@@ -2,16 +2,45 @@ from paddle import Paddle
 
 
 class EnemyAI(Paddle):
-    def __init__(self, position):
+    def __init__(self, position, hardness):
         super().__init__(position=position)
-        self.move_speed = 20
+        self.move_speed = 10
         self.color("red")
         self.goto(position)
+        self.hardness = hardness
     
     def move_up(self):
-        new_y = self.ycor() + 20
+        new_y = self.ycor() + 10
         self.goto(self.xcor(), new_y)
+        if self.hardness == "Easy":                
+            new_y = self.ycor()-1.5
+            self.goto(self.xcor(), new_y)
+
+        elif self.hardness == "Medium":                
+            new_y = self.ycor()-1
+            self.goto(self.xcor(), new_y)
+
+        elif self.hardness == "Hard":                
+            new_y = self.ycor()-0.5
+            self.goto(self.xcor(), new_y)
+
+        else:
+            pass
 
     def move_down(self):
-        new_y = self.ycor() - 20
+        new_y = self.ycor() - 10
         self.goto(self.xcor(), new_y)
+        if self.hardness == "Easy":                
+            new_y = self.ycor()+1.5
+            self.goto(self.xcor(), new_y)
+
+        elif self.hardness == "Medium":                
+            new_y = self.ycor()+1
+            self.goto(self.xcor(), new_y)
+
+        elif self.hardness == "Hard":                
+            new_y = self.ycor()+0.5
+            self.goto(self.xcor(), new_y)
+
+        else:
+            pass)
